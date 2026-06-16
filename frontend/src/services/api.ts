1d0607021ec6d6ad6a49ab2eb43b909ecda12cec
seba14558 <seba14558@gmail.com>
2026-06-16 20:05:39 -0300

    feat: subir cambios para despliegue

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://192.168.1.35:3001/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://yin-dipper-semester.ngrok-free.dev/api',
   headers: {
     'Content-Type': 'application/json',
   },
