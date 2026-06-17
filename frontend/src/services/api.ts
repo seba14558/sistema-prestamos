6d7c2a45bd212addfa9cc5960aceb2b5a131f981
seba14558 <seba14558@gmail.com>
2026-06-17 00:14:44 -0300

    Use localhost for local development

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://plenty-maps-poke.loca.lt/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001/api',
   headers: {
     'Content-Type': 'application/json',
   },
